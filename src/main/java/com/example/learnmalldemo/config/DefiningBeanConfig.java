a181e88cfd1afd776a4627b0ce93e35e66ece5d0
AhogeK <ahogek@gmail.com>
2021-04-14 11:24:55 +0800

    feat(校验): 添加校验配置

---
+++ src/main/java/com/example/learnmalldemo/config/DefiningBeanConfig.java
@@ -0,0 +1,35 @@
+package com.example.learnmalldemo.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+/**
+ * <p>
+ * 声明自定义Bean
+ * </p>
+ *
+ * @author AhogeK ahogek@gmail.com
+ * @date 2021-04-13 14:45
+ * @since 1.00
+ */
+@Configuration
+public class DefiningBeanConfig {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:message");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean getValidator() {
+        LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+        bean.setValidationMessageSource(messageSource());
+        return bean;
+    }
+}
