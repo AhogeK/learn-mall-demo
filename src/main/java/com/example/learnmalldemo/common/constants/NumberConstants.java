0064d2fa3ee8b68573d588bf8c2d26c4311d4451
AhogeK <ahogek@gmail.com>
2021-04-14 11:26:32 +0800

    feat(常量): 添加数字常量类

---
+++ src/main/java/com/example/learnmalldemo/common/constants/NumberConstants.java
@@ -0,0 +1,35 @@
+package com.example.learnmalldemo.common.constants;
+
+/**
+ * <p>
+ * 数字相关常量类
+ * </p>
+ *
+ * @author AhogeK ahogek@gmail.com
+ * @date 2021-04-14 11:04
+ * @since 1.00
+ */
+public class NumberConstants {
+
+    public static final Integer ZERO = 0;
+
+    public static final Integer ONE = 1;
+
+    public static final Integer TWO = 2;
+
+    public static final Integer THREE = 3;
+
+    public static final Integer FOUR = 4;
+
+    public static final Integer FIVE = 5;
+
+    public static final Integer SIX = 6;
+
+    public static final Integer SEVEN = 7;
+
+    public static final Integer EIGHT = 8;
+
+    public static final Integer NIGHT = 9;
+
+    public static final Integer TEN = 10;
+}
---
+++ src/main/java/com/example/learnmalldemo/form/VerifyAuthCodeForm.java
@@ -0,0 +1,29 @@
+package com.example.learnmalldemo.form;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * <p>
+ * 校验验证码
+ * </p>
+ *
+ * @author AhogeK ahogek@gmail.com
+ * @date 2021-04-13 14:20
+ * @since 1.00
+ */
+@Data
+@ApiModel("校验验证码表单参数实体")
+public class VerifyAuthCodeForm {
+
+    @NotBlank(message = "{notnull}")
+    @ApiModelProperty("手机号")
+    private String telephone;
+
+    @NotBlank(message = "{notnull}")
+    @ApiModelProperty("验证码")
+    private String authCode;
+}
