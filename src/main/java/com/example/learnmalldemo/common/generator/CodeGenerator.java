aba8a2da5e77283027659d8f5cfd956176dcc313
AhogeK <ahogek@gmail.com>
2021-04-15 22:59:50 +0800

    style(整体项目): 去除老mbg配置文件

--- src/main/java/com/example/learnmalldemo/common/generator/CodeGenerator.java
+++ src/main/java/com/example/learnmalldemo/common/generator/CodeGenerator.java
@@ -136,7 +136,7 @@ public class CodeGenerator {
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
         // 公共父类
-        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
+        // strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
         // 写于父类中的公共字段
         strategy.setSuperEntityColumns("id");
         strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
