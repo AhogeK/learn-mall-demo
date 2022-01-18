e21b139eb582183b49f3f9be159fc95490f64776
ahogek <ahogek@gmail.com>
2022-01-18 17:54:17 +0800

    style(单元测试): 描述添加

--- src/test/java/com/example/learnmalldemo/LearnMallDemoApplicationTests.java
+++ src/test/java/com/example/learnmalldemo/LearnMallDemoApplicationTests.java
@@ -4,6 +4,7 @@ import com.example.learnmalldemo.service.admin.PmsBrandTestService;
 import com.example.learnmalldemo.service.pms.IPmsBrandService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,6 +31,7 @@ class LearnMallDemoApplicationTests {
     }
 
     @Test
+    @DisplayName("品牌接口单元测试")
     void brandServiceTest() {
         PmsBrandTestService brandTest = new PmsBrandTestService(brandService);
         brandTest.listAllBrandTest();
