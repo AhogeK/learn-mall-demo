78b0299eff7ccc414465c3982f0b1c2b99b47fe2
ahogek <ahogek@gmail.com>
2022-02-08 19:18:09 +0800

    fix(SonarLint): MemberReadHistoryController class fix
    Annotation repetitions should not be wrapped

--- src/main/java/com/example/learnmalldemo/controller/mongo/MemberReadHistoryController.java
+++ src/main/java/com/example/learnmalldemo/controller/mongo/MemberReadHistoryController.java
@@ -8,7 +8,6 @@ import com.example.learnmalldemo.entity.mongo.MemberReadHistory;
 import com.example.learnmalldemo.service.mongo.IMemberReadHistoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
@@ -49,18 +48,17 @@ public class MemberReadHistoryController {
     @DeleteMapping
     @Parameter(name = "ids", description = "id数组", in = ParameterIn.QUERY, required = true, example = "1")
     public CommonResult<Void> removeBatchById(@RequestParam("ids") @Size(min = 1, message = "{not-zero}")
-                                              @NotNull(message = "{notnull}") String[] ids) {
+                                                  @NotNull(message = "{notnull}") String[] ids) {
         memberReadHistoryService.removeBatchById(ids);
         return CommonResult.success();
     }
 
     @Operation(summary = "简单搜索")
     @GetMapping
-    @Parameters({
-            @Parameter(name = "pageNum", description = "当前页", in = ParameterIn.QUERY, example = "1"),
-            @Parameter(name = "pageSize", description = "每页大小", in = ParameterIn.QUERY, example = "5")
-    })
-    public CommonResult<Page<MemberReadHistory>> page(@Parameter(hidden = true) @LoginUser UmsAdmin umsAdmin, @RequestParam(defaultValue = "1") Integer pageNum,
+    @Parameter(name = "pageNum", description = "当前页", in = ParameterIn.QUERY, example = "1")
+    @Parameter(name = "pageSize", description = "每页大小", in = ParameterIn.QUERY, example = "5")
+    public CommonResult<Page<MemberReadHistory>> page(@Parameter(hidden = true) @LoginUser UmsAdmin umsAdmin,
+                                                      @RequestParam(defaultValue = "1") Integer pageNum,
                                                       @RequestParam(defaultValue = "5") Integer pageSize) {
         return CommonResult.success(memberReadHistoryService.page(umsAdmin.getId(), pageNum - 1, pageSize));
     }
