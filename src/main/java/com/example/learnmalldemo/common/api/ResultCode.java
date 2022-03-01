31d96d8f554278b48a63b53f715991dc9ca8bbef
AhogeK <ahogek@gmail.com>
2022-03-01 17:20:19 +0800

    style(ResultCode): comment format

--- src/main/java/com/example/learnmalldemo/common/api/ResultCode.java
+++ src/main/java/com/example/learnmalldemo/common/api/ResultCode.java
@@ -15,15 +15,15 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ResultCode {
-    /*
+    /**
      * 操作成功
      */
     SUCCESS(200, "操作成功"),
-    /*
+    /**
      * 操作失败
      */
     FAILED(500, "操作失败"),
-    /*
+    /**
      * 新增操作失败
      */
     INSERT_FAILED(500, "新增操作失败"),
@@ -43,7 +43,7 @@ public enum ResultCode {
      * HTTP请求参数不可读
      */
     MESSAGE_NOT_READABLE_FAILED(500, "HTTP请求参数不可读"),
-    /*
+    /**
      * 参数校验失败
      */
     VALIDATE_FAILED(404, "参数检验失败"),
@@ -51,11 +51,11 @@ public enum ResultCode {
      * 验证码校验失败
      */
     AUTH_CODE_VALIDATE_FAILED(404, "验证码校验失败"),
-    /*
+    /**
      * token过期
      */
     UNAUTHORIZED(401, "暂未登录或token已经过期"),
-    /*
+    /**
      * 没有相关权限
      */
     FORBIDDEN(403, "没有相关权限");
